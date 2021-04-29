ca8435778a9b1dba057e55a79a52d46973bd9b6e
Fabián <fabian@nano.org.ve>
2021-04-29 05:13:06 -0400

    My website

---
+++ app.js
@@ -0,0 +1,9 @@
+// Needs to make a function to get rid of the preloader
+
+window.addEventListener("load", () => {
+    const preload = document.querySelector(".preload");
+    preload.classList.add("preload-finish");
+});
+
+// Cursor
+
